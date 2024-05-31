4874b892524124b376eb33f599b6c48ac95275d3
naylabilla <naylabilla3@gmail.com>
2024-05-31 11:14:30 +0700

    Praktikum Proweb M11

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Produk;
+
+class ApiController extends Controller
+{
+    public function index()
+    {
+        $numbers = range(1, 100); // Membuat array berisi angka 1 sampai 100
+        return response()->json($numbers);
+    }
+
+    public function getProduct()
+    {
+        $data = Produk::all();
+        return response()->json($data);
+    }
+}
